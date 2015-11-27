c2621aa012b485baff74c193effe0c32150a4d9d
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-27 11:31:10 +0000

    use setup method for tests

--- src/EchoServerTest.java
+++ src/EchoServerTest.java
@@ -1,12 +1,20 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class EchoServerTest {
 
+    private EchoServer echoServer;
+    private FakeConsole fakeConsole;
+
+    @Before
+    public void setup() {
+        fakeConsole = new FakeConsole();
+        echoServer = new EchoServer(fakeConsole);
+    }
+
     @Test
     public void printMessageToConsole() {
-        FakeConsole fakeConsole = new FakeConsole();
-        EchoServer echoServer = new EchoServer(fakeConsole);
         echoServer.print("hello");
         assertEquals("hello", fakeConsole.messagePrinted());
     }
